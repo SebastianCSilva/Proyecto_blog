2fd00da420e8cd92fdb00a589dda8b6c833ed451
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-08-29 23:27:21 -0400

    se agregan templates, se crea la tabla de los post se modifica settings del proyecto

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+
 def home(request):
     return render(request, 'blog/home.html')
+    
+def about(request):
+    return render(request, 'blog/about.html')
