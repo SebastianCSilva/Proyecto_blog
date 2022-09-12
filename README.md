bddd928b275b1c733693b88fea24a72a9bdc9d1b
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-09-12 00:41:07 -0300

    se crea la api basica del blog para ver los posts y los detalles de posts

---
+++ README.md
@@ -0,0 +1,7 @@
+Para utilizar y subir este proyecto que fue creado con los siguientes
+
+Django==3.2.15
+django-crispy-forms==1.14.0
+djangorestframework==3.13.1
+psycopg2==2.9.3
+
--- apipost/views.py
+++ apipost/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 # Create your views here.
+# PostSerializer
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
